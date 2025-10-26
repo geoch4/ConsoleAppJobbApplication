b582c2bf49ab909494f851867edc6af01c278fba
Georgia Chalkia <grchalkia@gmail.com>
2025-10-26 22:02:51 +0100

    Har uppdaterat JobManager klassen

--- JobManager.cs
+++ JobManager.cs
@@ -9,7 +9,7 @@ namespace ConsoleAppJobbApplication
     public class JobManager
     {
         //JobManager – ansvarar för att hantera alla ansökningar (listan, logik och LINQ).
-        public List<JobApplication> Applications = new List<JobApplication>();
+        public List<JobApplication> Applications = new List<JobApplication>();//skulle vi kunna ha en lista som en variabel?
 
 
         //Lägg till från redan skapad class JobApplication så att listan tar info från class JobApplication
@@ -23,7 +23,7 @@ namespace ConsoleAppJobbApplication
 
 
         //Visning av alla ansökningar
-        public  void ShowAllApplication() //vi döper metoden ShowAllApplication för att förstå vad metoden gör
+        public void ShowAllApplications() //vi döper metoden ShowAllApplication för att förstå vad metoden gör
         {
 
             if (Applications.Count == 0) //== betyder "lika med"-exakt-annat sätt att formulera i kodform?
@@ -45,7 +45,7 @@ namespace ConsoleAppJobbApplication
             Console.WriteLine("Ange företagsnamn för att uppdatera status:");
             string companyName = Console.ReadLine();
 
-            foreach(var application in Applications)
+            foreach (var application in Applications)
             {
                 if (application.CompanyName.ToLower() == companyName.ToLower())
                 {
@@ -53,11 +53,11 @@ namespace ConsoleAppJobbApplication
                     Console.WriteLine("Status alternativ: 0=Applied, 1= Interview, 2=Offer, 3=Rejected");
 
                     int statusChoice;
-                    while(true)
+                    while (true)
                     {
                         Console.WriteLine("Välj ny status");
-                        if(int.TryParse(Console.ReadLine(), out statusChoice) &&
-                            Enum.IsDefined(typeof(JobApplication.Status), statusChoice))
+                        if (int.TryParse(Console.ReadLine(), out statusChoice) &&
+                            Enum.IsDefined(typeof(Status), statusChoice))
                         {
                             break;
                         }
@@ -67,12 +67,12 @@ namespace ConsoleAppJobbApplication
                         }
                     }
 
-                    application.ApplicationStatus = (JobApplication.Status)statusChoice;
+                    application.ApplicationStatus = (Status)statusChoice;
                     Console.WriteLine($"Status uppdaterad för {companyName} till {application.ApplicationStatus}");
                 }
             }
 
-            
+
         }
 
         public void ShowStatistics() //får utgå ifrån att det är statistik för antal ansökningar
@@ -97,12 +97,13 @@ namespace ConsoleAppJobbApplication
             }
 
             //genomsnittligt svarstid
-            var answered = Applications.Where(application => application.ResponseDate != null);//filtrerar bort de utan svarsdatum
+            var answered = Applications.Where(application => application.ResponseDate != default);//filtrerar bort de utan svarsdatum
+            //det kan inte vara samma med null
 
             if (answered.Any())
             {
-                var averageResponseDays = answered.Average(application=> (application.ResponseDate - application.ApplicationDate).TotalDays);
-                   
+                var averageResponseDays = answered.Average(application => (application.ResponseDate - application.ApplicationDate).TotalDays);
+
                 Console.WriteLine($"\nGenomsnittlig svarstid: {averageResponseDays} dagar");
             }
             else
@@ -111,15 +112,17 @@ namespace ConsoleAppJobbApplication
             }
         }
 
+
+
         public void ShowByStatus()
         {  //status kommer från användaren som skriver t.ex. applied)
             Console.WriteLine("Ange status för filtrering(0=Applied, 1=Interview, 2=Offer, 3=Rejected):");
             Console.WriteLine("Ange siffra 0-3");
-          
+
             int choice = int.Parse(Console.ReadLine()); //skapar en lokal variabel-choice
-            JobApplication.Status status = (JobApplication.Status)choice; //denna raden för att vi ska kunna relatera användarens val
-                                                                          //till status-syftet med det är att vi ska vara medvetna om vad
-                                                                          //valet(choice) handlar om
+            Status status = (Status)choice; //denna raden för att vi ska kunna relatera användarens val
+                                            //till status-syftet med det är att vi ska vara medvetna om vad
+                                            //valet(choice) handlar om
 
 
 
@@ -137,16 +140,91 @@ namespace ConsoleAppJobbApplication
                 return; //returnerar svaret jag får
             }
             //3. annars visa sammanfattning för varje ansökan i resultatet-skriv ut träffarna
-            
+
             else foreach (var app in resultOfList)
-            {
+                {
                     Console.WriteLine($"Ansökningar med status:");
                     Console.WriteLine(app.GetSummary());
-                   
-                }
 
+                }
+        }
+
+         
+        public void ShowSortedByDate(bool ascending)
+        {
+            IEnumerable<JobApplication> ordered = ascending
+                ? Applications.OrderBy(app => app.ApplicationDate)
+                : Applications.OrderByDescending(app => app.ApplicationDate);
+
+              var list = ordered.ToList();
+            if(list.Count == 0 )
+            {
+                Console.WriteLine("inga ansökningar att visa");
+                return;
+            }
+            foreach(var app in ordered)
+            {
+                    Console.WriteLine(app.GetSummary());
+            }
 
         }
-    }
+
+        public void RemoveByCompany(string company)
+        {
+            if(string.IsNullOrWhiteSpace(company))
+            {
+                Console.WriteLine("Företaget finns inte.");
+                return;
+            }
+            int removed = Applications.RemoveAll(app =>
+            app.CompanyName.Equals(company, StringComparison.OrdinalIgnoreCase));
+
+            if(removed == 0)
+            {
+                Console.WriteLine("inga ansökningar hitttades för att ta bort");
+            }
+            else
+            {
+                Console.WriteLine($"Tog bort {removed} ansökan för företager {company}");
+            }
+
+        }
+
+        public void AddJob()
+        {
+            // ApplyForJob() ska ha signaturen: public static JobApplication ApplyForJob()
+            var app = JobApplication.ApplyForJob();
+            Applications.Add(app);
+            Console.WriteLine("Ansökan är tillagd");
+        }
+
+
+        public void SortApplications()
+        {
+            Console.WriteLine("Hur vill du sortera");
+            Console.WriteLine("1. Äldst till nyast");
+            Console.WriteLine("2. Nyast till äldst");
+            Console.WriteLine("Välj 1 eller 2");
+
+            string val = Console.ReadLine();
+
+            bool ascending = (val == "1");
+
+           
+
+        }
+
+        private void RemoveJob()
+        {
+            Console.Write("Ange företagsnamn att ta bort: ");
+            string company = Console.ReadLine();
+            RemoveByCompany(company);
+        }
+    } 
+   
 
 }
+
+        
+    
+
